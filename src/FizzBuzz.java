747b80a842475876ba118d85c2bb837ecc635f80
DesireeGon <ren.desireegon@gmail.com>
2023-03-29 23:08:23 +0200

    Test con el test 1 revisando con los números 1 y 2, y test 3 revisando el número 3

---
+++ src/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public String FizzBuzz_Calculo (int pomelo) {
+
+    String FizzBuzz;
+        if (pomelo %3 == 0) {
+            FizzBuzz = "Fizz";
+        } else {
+            String platano = String.valueOf(pomelo);
+            return platano;
+        }
+        return FizzBuzz;
+    }
+}
